7edce1cacd6bc754cf6bf68018ae827fe41dbed8
Nestorius01 <nestorccosme@gmail.com>
2025-05-22 19:01:56 +0200

    Subida de archivos
    Es una prueba que sirve para tener un ejemplo de cómo subir una página a GitHub y activarla online

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const formulario = document.getElementById("formulario-basico");
+    const resultado = document.getElementById("resultado");
+
+
+});
+
+
+
+
+
